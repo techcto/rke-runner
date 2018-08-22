69758683690b127ca69c4758ff2a7a49d99f08f2
ocoa <shawn@ocoa.com>
2018-08-22 09:05:42 -0400

    change build info

--- cmd.sh
+++ cmd.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 rkeClean() {
-    python-lambda-local -l ~/.local/lib/ -e env.clean.json -f run -t 1500 app.py event.json
+    python-lambda-local -l ~/.local/lib/ -e env.clean.json -f run -t 3500 app.py event.json
 }
 
 rkeUpdate() {
-    python-lambda-local -l ~/.local/lib/ -e env.json -f run -t 1500 app.py event.json
+    python-lambda-local -l ~/.local/lib/ -e env.json -f run -t 3500 app.py event.json
 }
 
 $*
